e825b0b7b994088ec741a5926545fe5771516fb7
Justin Vrana <justin.vrana@gmail.com>
2020-05-13 09:43:56 -0700

    Aquadoc created file

---
+++ js/definitions.js
@@ -0,0 +1,185 @@
+var config = {
+
+  tagline: "The Laboratory</br>Operating System",
+  documentation_url: "http://localhost:4000/aquarium",
+  title: "My Workflow",
+  navigation: [
+
+    {
+      category: "Overview",
+      contents: [
+        { name: "Introduction", type: "local-md", path: "README.md" },
+        { name: "About this Workflow", type: "local-md", path: "ABOUT.md" },
+        { name: "License", type: "local-md", path: "LICENSE.md" },
+        { name: "Issues", type: "external-link", path: 'https://github.com/klavinslab/OLA-Simple/issues' }
+      ]
+    },
+
+    
+
+      {
+
+        category: "Operation Types",
+
+        contents: [
+
+          
+            {
+              name: 'Assign Synthetic DNA for Kit',
+              path: 'operation_types/Assign_Synthetic_DNA_for_Kit' + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'OLASimple Cell Lysis',
+              path: 'operation_types/OLASimple_Cell_Lysis' + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'OLASimple Interactive Survey',
+              path: 'operation_types/OLASimple_Interactive_Survey' + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'OLASimple Ligation',
+              path: 'operation_types/OLASimple_Ligation' + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'OLASimple PCR',
+              path: 'operation_types/OLASimple_PCR' + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'OLASimple Paper Detection',
+              path: 'operation_types/OLASimple_Paper_Detection' + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'OLASimple Tests',
+              path: 'operation_types/OLASimple_Tests' + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'OLASimple Visual Call',
+              path: 'operation_types/OLASimple_Visual_Call' + '.md',
+              type: "local-md"
+            },
+          
+
+        ]
+
+      },
+
+    
+
+    
+
+      {
+
+        category: "Libraries",
+
+        contents: [
+
+          
+            {
+              name: 'OLAConstants',
+              path: 'libraries/OLAConstants' + '.html',
+              type: "local-webpage"
+            },
+          
+            {
+              name: 'OLAGraphics',
+              path: 'libraries/OLAGraphics' + '.html',
+              type: "local-webpage"
+            },
+          
+            {
+              name: 'OLAKits',
+              path: 'libraries/OLAKits' + '.html',
+              type: "local-webpage"
+            },
+          
+            {
+              name: 'OLALib',
+              path: 'libraries/OLALib' + '.html',
+              type: "local-webpage"
+            },
+          
+            {
+              name: 'SVGGraphics',
+              path: 'libraries/SVGGraphics' + '.html',
+              type: "local-webpage"
+            },
+          
+
+        ]
+
+    },
+
+    
+
+    
+      { category: "Sample Types",
+        contents: [
+          
+            {
+              name: 'OLASimple Sample',
+              path: 'sample_types/OLASimple_Sample'  + '.md',
+              type: "local-md"
+            },
+          
+        ]
+      },
+      { category: "Containers",
+        contents: [
+          
+            {
+              name: 'OLA Detection Strips',
+              path: 'object_types/OLA_Detection_Strips'  + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'OLA Ligation Stripwell',
+              path: 'object_types/OLA_Ligation_Stripwell'  + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'OLA PCR',
+              path: 'object_types/OLA_PCR'  + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'OLA Whole Blood',
+              path: 'object_types/OLA_Whole_Blood'  + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'OLA lysed blood',
+              path: 'object_types/OLA_lysed_blood'  + '.md',
+              type: "local-md"
+            },
+          
+            {
+              name: 'OLASimple Synthetic DNA',
+              path: 'object_types/OLASimple_Synthetic_DNA'  + '.md',
+              type: "local-md"
+            },
+          
+        ]
+      }
+    
+
+  ]
+
+};
